58a651d36b8ab1a996ccc8d60dc8f175bbe3139c
BounyMathieu <mathieu.bouny@edu.esiee.fr>
2024-09-20 09:12:57 +0200

    main.py VERSION FINALE

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 #### Fonction secondaire
+'''
+Fonction Palyndromes
+'''
 
+def ispalindrome(s):
+    '''
+    Determine si 's' est une palindrome ou non.
 
-def ispalindrome(p):
-
+    Args:
+        s : chaine de caracteres
+    Returns:
+        ispalyndrome(s) : 's est un palyndrome' / 's n'est pas un palyndrome'
+    '''
+    if len(s)<=1:
+        return(s, 'est un palyndrome')
+    for i in range (len(s)//2):
+        if s[i]!=s[-(i+1)]:
+            return (s,"n'est pas un palyndrome")
+    return (s,'est un palyndrome')
     # votre code ici
-    
-    return False
-
 #### Fonction principale
 
 
 def main():
-
+    '''
+    Détermine si la fonction ispalyndrome() fonctionne bien pour les mots suivants :
+    "radar", 
+    "kayak", 
+    "level", 
+    "rotor", 
+    "civique", 
+    "deifie".
+    '''
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
+    print()
 
 
 if __name__ == "__main__":
